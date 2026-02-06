d652e71d3a7970d94f776349c76adbe97d4bb04f
AMRIT KUMAR GORAI <85100180+amritk32@users.noreply.github.com>
2026-02-06 21:12:07 +0530

    recursion2.cpp

--- Linear Data Structures/Arrays/Recursion/recursion2.cpp
+++ Linear Data Structures/Arrays/Recursion/recursion2.cpp
@@ -106,12 +106,84 @@ void getPermutation(vector<int> &arr, vector<int> &nums , int index , vector<vec
 
 }
 
+class Solution {
+private:
+    void helper(string &tiles, vector<bool> &visited, int &count, string &current) {
+       if(!current.empty()){
+        count++;
+       }
+        unordered_set<char> seen;
+
+        for (int i = 0; i < tiles.size(); i++) {
+            
+            if (visited[i] || seen.count(tiles[i])) {
+                continue;
+            }
+
+            // Mark as used
+            seen.insert(tiles[i]);
+            visited[i] = true;
+            current.push_back(tiles[i]);
+          
+
+            // Recurse to fill the next position
+            helper(tiles, visited, count, current);
+
+            // Backtrack: Reset for the next iteration of the loop
+            current.pop_back();
+            visited[i] = false;
+        }
+    }
+
+public:
+    int numTilePossibilities(string tiles) {
+        int count = 0;
+        vector<bool> visited(tiles.size(), false);
+        string current = "";
+        
+        helper(tiles, visited, count, current);
+        
+        return count;
+    }
+};
+
+void permute(vector<int> &arr, vector<vector<int>> &ans, vector<int> &current, vector<bool> &used){
+    // Base case
+    if(current.size() == arr.size()){
+        ans.push_back(current);
+        return;
+    }
+
+    for(int i = 0; i < arr.size(); i++){
+        if(!used[i]){
+
+            // Inclusion
+            current.push_back(arr[i]);
+            used[i] = true;
+
+            // Recursive call
+            permute(arr, ans, current, used);
+
+            // Exclusion
+            current.pop_back();
+            used[i] = false;
+
+            
+        }
+    }
+
+
+}
+
 // use for loop when multiple parameters compete for the same place
 // Use inclusion exclusion principle without for loop when vector does not have duplicates
 // Use inclusion exclusion principle with for loop when vector contains duplicates then manual sorting is required(mergesort)
 // To use exactly each number once in a combination question use looping method and in next recursive call for i + 1 instead of index + 1 and start
 // i from index in the for loop
 
+// Use proper method for permutations depending upon the question
+
 // Use set to determine duplicates when sorting + skipping is too complex 
 
 // Use sorting and defining duplicates when using set increases space complexity
+
